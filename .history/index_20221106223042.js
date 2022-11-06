40211573eaa013f7ec203bbcfd7eecf4032cf6ad
heartbreathing <li.wang@hyperisland.se>
2022-11-06 23:20:59 +0100

    search_weather

---
+++ .history/index_20221106223042.js
@@ -0,0 +1,27 @@
+    "use strict";
+      let form = document.querySelector('.form');
+      let city;
+
+      async function getData() {
+        let res = await fetch(
+          `https://goweather.herokuapp.com/weather/${city}`
+        );
+        const data = await res.json();
+
+        const temp = document.querySelector('.temperature');
+        const wind = document.querySelector('.wind');
+        const forecast = document.querySelector('.content');
+        const description = document.querySelector('.description');
+
+        temp.innerHTML = data.temperature;
+        wind.innerHTML = data.wind;
+        description.innerHTML = data.description;
+        name
+      }
+
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        city = this.elements.name.value;
+        getData();
+      });
+   
---
+++ .history/index_20221106230304.js
@@ -0,0 +1,39 @@
+    "use strict";
+      let timeEl = document.querySelector('.time')
+
+      function getTime()
+    //   function getTime() {
+    //     timeEl.innerHTML = new Date().toDateString();
+    //     // setTimeout(getTime, 1000);
+    //   }
+    //   getTime();
+
+      
+      let form = document.querySelector('.form');
+      let city;
+
+      async function getData() {
+        let res = await fetch(
+          `https://goweather.herokuapp.com/weather/${city}`
+        );
+        const data = await res.json();
+
+        const temp = document.querySelector('.temperature');
+        const wind = document.querySelector('.wind');
+        const forecast = document.querySelector('.content');
+        const description = document.querySelector('.description');
+        // const name = document.querySelector('.name')
+
+        temp.innerHTML = data.temperature;
+        wind.innerHTML = data.wind;
+        description.innerHTML = data.description;
+        // name.innerHTML = city
+        
+      }
+
+      form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        city = this.elements.name.value;
+        getData();
+      });
+   
